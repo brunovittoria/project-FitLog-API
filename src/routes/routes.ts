b9d8a57a1f5dc259bc821c3914907f345a6a14cd
Bruno Vittoria <brunovittoria1999@hotmail.com>
2024-08-21 04:51:28 -0300

    FEAT:Login functionality ready

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
 import express, { Router, Request, Response } from 'express'
 
 import { CreateUserController } from '../controllers/user/CreateUserController'
+import { AuthUserController }   from '../controllers/user/AuthUserController'
 
 const router = Router()
 
@@ -12,7 +13,8 @@ const router = Router()
 */
 
 // --- ROTAS USER --- \\
-router.post('/users', new CreateUserController().handle)
+router.post('/users', new CreateUserController().handle)                            //CADASTRO DE USERS. a lógica para criar usuários está encapsulada no método handle do CreateUserController. Provavelmente, esse método irá extrair os dados da requisição (por exemplo, nome, e-mail, senha), criar um novo usuário e responder de volta ao cliente com os resultados da operação.
+router.post('/session', new AuthUserController().handle)                            //Com essa rota ira fazer o login
 
 
 
