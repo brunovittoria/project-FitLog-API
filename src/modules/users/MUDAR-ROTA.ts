4e035598d284d571f1f011648994cfd6d1df397f
brunovittoria <brunogun1999@gmail.com>
2024-10-03 19:17:09 -0300

    FEAT: Updated app Architecture

---
+++ src/modules/users/MUDAR-ROTA.ts
@@ -0,0 +1,21 @@
+import express, { Router, Request, Response } from 'express'
+
+import { CreateUserController } from './createUser/CreateUserController'
+import { AuthUserController } from './Authentication/AuthUserController'
+
+import { isAuthenticated } from '../../middlewares/isAuth'
+
+const router = Router()
+
+/*Rota teste Inicial
+    router.get('/test', (req: Request, res: Response) => {
+        throw new Error("TESTE DE ERRO AQUI")     
+        return res.json({ ok: true })
+    })
+*/
+
+// --- ROTAS USER --- \\
+router.post('/users', new CreateUserController().handle) //CADASTRO DE USERS. a lógica para criar usuários está encapsulada no método handle do CreateUserController. Provavelmente, esse método irá extrair os dados da requisição (por exemplo, nome, e-mail, senha), criar um novo usuário e responder de volta ao cliente com os resultados da operação.
+router.post('/session', new AuthUserController().handle) //Com essa rota ira fazer o login
+
+export { router }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express'
-import { router } from './routes/routes'
+import { router } from './modules/users/MUDAR-ROTA'
 import 'express-async-errors'
 import cors from 'cors'
 
@@ -8,11 +8,11 @@ import cors from 'cors'
 const app = express()
 
 app.use((req, res, next) => {
-    if(req.originalUrl === '/webhooks'){
-        next()
-    }else{
-        express.json()(req, res, next)
-    }
+  if (req.originalUrl === '/webhooks') {
+    next()
+  } else {
+    express.json()(req, res, next)
+  }
 })
 
 app.use(cors()) //Assim liberamos pra qualquer um fazer e enviar req, poderiamos limitar tb o uso da API por IP usando o CORS
@@ -20,17 +20,19 @@ app.use(cors()) //Assim liberamos pra qualquer um fazer e enviar req, poderiamos
 app.use(router)
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    if(err instanceof Error){                                                 //Erros do tipo que nos tratamos
-        return res.status(400).json({
-            error: err.message
-        })
-    }
-
-    return res.status(500).json({                                            //Erros do tipo que o servidor nos envia
-        status: 'error',
-        message: 'Internal server error'
+  if (err instanceof Error) {
+    //Erros do tipo que nos tratamos
+    return res.status(400).json({
+      error: err.message
     })
+  }
+
+  return res.status(500).json({
+    //Erros do tipo que o servidor nos envia
+    status: 'error',
+    message: 'Internal server error'
+  })
 })
 //Aqui abaixo teremos nosso index.js rotas que se ocupara de todas as rotas:
 
-app.listen(3333 , () => console.log("Server is running") )
+app.listen(3333, () => console.log('Server is running'))
